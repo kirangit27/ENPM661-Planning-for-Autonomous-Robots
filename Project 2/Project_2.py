765f0ae0e772b9c8bcf9457eeebbed25af5552ab
kirangit27 <kiran.suvas.patil@gmail.com>
2023-03-12 13:00:22 -0400

    modified Obstacle_Area(), added clearance

--- Project 2/Project_2.py
+++ Project 2/Project_2.py
@@ -15,11 +15,17 @@ def Obstacle_Area():
     canvas = blank_canvas.copy()
 
     #Rectangle 1
+    pts = np.array([[100-clearance,y_invert-0+clearance],[100-clearance,y_invert-100-clearance], [150+clearance,y_invert-100-clearance], [150+clearance,y_invert-0+clearance]], np.int32)
+    pts = pts.reshape((-1,1,2))
+    cv2.fillPoly(canvas,[pts],(255,255,255))
     pts = np.array([[100,y_invert-0],[100,y_invert-100], [150,y_invert-100], [150,y_invert-0]], np.int32)
     pts = pts.reshape((-1,1,2))
     cv2.fillPoly(canvas,[pts],(0, 255, 0))
 
     #Rectangle 2
+    pts = np.array([[100-clearance,0-clearance],[100-clearance,100+clearance], [150+clearance,100+clearance], [150+clearance,0-clearance]], np.int32)
+    pts = pts.reshape((-1,1,2))
+    cv2.fillPoly(canvas,[pts],(255,255,255))
     pts = np.array([[100,0],[100,100], [150,100], [150,0]], np.int32)
     pts = pts.reshape((-1,1,2))
     cv2.fillPoly(canvas,[pts],(0, 255, 0))
@@ -27,11 +33,20 @@ def Obstacle_Area():
     #Hexagon
     a = 75
     h = int((math.sqrt(3)/2)*a)
+    a_ = clearance/math.sqrt(2)
+    pts = np.array([[300-h-a_,125-a/2-a_],[300-h-a_,125+a/2+a_],[300,125+a+clearance],[300+h+a_,125+a/2+a_],[300+h+a_,125-a/2-a_],[300,125-a-clearance]], np.int32)
+    pts = pts.reshape((-1,1,2))
+    cv2.fillPoly(canvas,[pts],(255,255,255))
     pts = np.array([[300-h,125-a/2],[300-h,125+a/2],[300,125+a],[300+h,125+a/2],[300+h,125-a/2],[300,125-a]], np.int32)
     pts = pts.reshape((-1,1,2))
     cv2.fillPoly(canvas,[pts],(0, 255, 0))
 
     #Triangle
+    theta = 63.43
+    pts = np.array([[460-clearance*math.cos(np.deg2rad(theta)),225+clearance*math.sin(np.deg2rad(theta))],[460-clearance*math.cos(np.deg2rad(theta)),25-clearance*math.sin(np.deg2rad(theta))],[510+clearance,125]], np.int32)
+    pts = np.array([[460-clearance,225+clearance*4],[460-clearance,25-clearance*4],[510+clearance,125]], np.int32)
+    pts = pts.reshape((-1,1,2))
+    cv2.fillPoly(canvas,[pts],(255,255,255))
     pts = np.array([[460,225],[460,25],[510,125]], np.int32)
     pts = pts.reshape((-1,1,2))
     cv2.fillPoly(canvas,[pts],(0, 255, 0))
